9a5266851728994bf55a6c4b5ec912ef70d1fc9a
Zeynab Jalalian <zjalalianweb@gmail.com>
2025-05-27 21:41:04 +0330

    js 1

---
+++ script.js
@@ -0,0 +1,9 @@
+let randomNumber = Math.floor(Math.random() * 100) + 1;
+
+let submit = document.querySelector("#submit");
+let input = document.querySelector("#guessField");
+let guesses = document.querySelector(".guesses");
+let remaining = document.querySelector(".lastResult");
+let lowOrHigh=document.querySelector(".lowOrHigh");
+let result=document.querySelector(".result");
+
