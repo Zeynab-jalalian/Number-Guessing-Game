f924420742f132731bb02115d581174da4a75542
Zeynab Jalalian <zjalalianweb@gmail.com>
2025-05-28 14:37:26 +0330

    first part js

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 const btn = document.getElementById("btn");
 let chancesText = document.getElementById("chances");
+let promptResult=document.querySelector("#prompt-result")
 let random = Math.floor(Math.random() * 100 + 1);
 let chances = 10;
- chancesText.innerHTML="chances:" + chances;
+chancesText.innerHTML = "chances:" + chances;
+
+
 btn.addEventListener("click", () => {
-    chances--;
-    chancesText.innerHTML="chances:" + chances;
+     chances--;
+    chancesText.innerHTML = "chances:" + chances;
+    if(chances==0){
+        activateBox("Loss");
+    }
 })
+function activateBox(e){
+promptResult.classList.add("active");
+}
