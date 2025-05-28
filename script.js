f929a26e5da527c081c3ac09ebe2245c57d8370b
Zeynab Jalalian <zjalalianweb@gmail.com>
2025-05-28 16:10:08 +0330

    music

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
+
 const btn = document.getElementById("btn");
 let chancesText = document.getElementById("chances");
-let number=document.getElementById("number");
-let result=document.getElementById("result");
-let txt=document.getElementById("txt");
+let number = document.getElementById("number");
+let result = document.getElementById("result");
+let txt = document.getElementById("txt");
 let promptResult = document.querySelector("#prompt-result")
 let random = Math.floor(Math.random() * 100 + 1);
 let chances = 10;
 let numberValue;
 chancesText.innerHTML = "chances:" + chances;
+const bgMusic = document.getElementById("bg-music");
+bgMusic.volume = 0.3;
+let musicStarted = false;
+
 
 
 btn.addEventListener("click", (e) => {
@@ -17,30 +22,44 @@ btn.addEventListener("click", (e) => {
     if (chances == 0) {
         activateBox("Loss");
     }
-    numberValue=number.value;
+    numberValue = number.value;
     check(numberValue);
-    number.value='';
+    number.value = '';
+
+    if (!musicStarted) {
+        bgMusic.play().catch(error => {
+            console.log("Error");
+        });
+        musicStarted = true;
+    }
 })
 
-function check(numberValue){
- if(numberValue==random){
-    activateBox("Win");
- }else if(numberValue>random){
- txt.innerHTML="Enter Small Number"
- }else{
-    txt.innerHTML="Enter Large Number"
- }
+function check(numberValue) {
+    if (numberValue == random) {
+        activateBox("Win");
+    } else if (numberValue > random) {
+        txt.innerHTML = "Enter Small Number"
+    } else {
+        txt.innerHTML = "Enter Large Number"
+    }
 }
+
 
 function activateBox(e) {
     promptResult.classList.add("active");
-    result.innerHTML=" You " + e + " The Game ";
-    document.body.classList.add("prompt-active");
+    if (e === "Win") {
+        result.innerHTML = "You won... but this is just the beginning.";
+    } else if (e === "Loss") {
+        result.innerHTML = "You failed. This game is not for you.";
+    }
 }
-function play(e){
+
+function play(e) {
     promptResult.classList.remove("active");
-    chances=10;
+    chances = 10;
     chancesText.innerHTML = "chances:" + chances;
-    txt.innerHTML="";
+    txt.innerHTML = "";
     document.body.classList.remove("prompt-active");
 }
+
+
