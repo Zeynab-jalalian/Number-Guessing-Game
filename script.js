5447919edddd5073e4be5d230294ed8aeb6e39af
Zeynab Jalalian <zjalalianweb@gmail.com>
2025-05-28 15:01:08 +0330

    js finished

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 const btn = document.getElementById("btn");
 let chancesText = document.getElementById("chances");
 let number=document.getElementById("number");
+let result=document.getElementById("result");
+let txt=document.getElementById("txt");
 let promptResult = document.querySelector("#prompt-result")
 let random = Math.floor(Math.random() * 100 + 1);
 let chances = 10;
+let numberValue;
 chancesText.innerHTML = "chances:" + chances;
 
 
@@ -14,13 +17,28 @@ btn.addEventListener("click", (e) => {
     if (chances == 0) {
         activateBox("Loss");
     }
+    numberValue=number.value;
+    check(numberValue);
+    number.value='';
 })
+
+function check(numberValue){
+ if(numberValue==random){
+    activateBox("Win");
+ }else if(numberValue>random){
+ txt.innerHTML="Enter Small Number"
+ }else{
+    txt.innerHTML="Enter Large Number"
+ }
+}
 
 function activateBox(e) {
     promptResult.classList.add("active");
+    result.innerHTML=" You " + e + " The Game ";
 }
 function play(e){
     promptResult.classList.remove("active");
     chances=10;
     chancesText.innerHTML = "chances:" + chances;
+    txt.innerHTML="";
 }
